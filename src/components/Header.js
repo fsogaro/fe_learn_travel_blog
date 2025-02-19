8d9acba17d1d40d4eb8668fdf9082f0b10f5da86
Francesca Sogaro <francesca.sogaro@stellantis.com>
2025-02-19 13:45:45 +0100

    static element design and css

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+
+export default function Header() {
+    return (
+        <header>
+            <div className="header-div">
+                <img src="globe.png" alt="globe logo"/>
+                <span>My Travel Journal</span>
+            </div>
+        </header>  
+    );
+};
