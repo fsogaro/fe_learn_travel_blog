7824b7e9682ef36b593d4db4635414141364f55a
Francesca Sogaro <francesca.sogaro@stellantis.com>
2025-02-19 15:01:23 +0100

    begin with props

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -14,20 +14,19 @@
  *   this data into the component.
  */
 
-export default function Entry(){
-    const url_link = "https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+export default function Entry(props){
     return(
         <article>
             <div className="main-img-container">        
-                <img src="https://scrimba.com/links/travel-journal-japan-image-url" width="100px" alt="main image"/>
+                <img src={props.img} width="100px" alt={props.alt_img}/>
             </div>
             <div className="info-container">
                 <img src="marker.png" alt ="location marker"/>
-                <span>Japan</span>
-                <a href={url_link}>View on Google Maps</a>
-                <h1>Mount Fuji</h1>
-                <h2>12 Jan, 2023 - 24 Jan, 2023</h2>
-                <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <span>{props.country}</span>
+                <a href={props.url_link}>View on Google Maps</a>
+                <h1>{props.title}</h1>
+                <h2>{props.dates}</h2>
+                <p>{props.text}</p>
             </div>
         </article>
 
